bb4430024d641d5a6f5ad5aaaa78978748d2a8d0
melody <melody@unplug.red>
2021-05-08 22:45:17 +0300

    added presets to plastic funeral

--- Plastic Funeral/source/Source/PluginProcessor.cpp
+++ Plastic Funeral/source/Source/PluginProcessor.cpp
@@ -27,84 +27,60 @@ FmerAudioProcessor::FmerAudioProcessor()
 	apvts(*this, &undoManager, "Parameters", createParameters())
 {
 	presets[0].name = "Default";
-	presets[0].freq = 0.3f;
+	presets[0].freq = 0.32f;
 	presets[0].fat = 0.0f;
 	presets[0].drive = 0.0f;
 	presets[0].dry = 0.0f;
-	presets[0].stereo = 0.3f;
-	presets[0].gain = 0.4f;
+	presets[0].stereo = 0.37f;
 
-	presets[1].name = "Preset 1";
-	presets[1].freq = 0.3f;
-	presets[1].fat = 0.0f;
-	presets[1].drive = 0.0f;
+	presets[1].name = "Digital Driver";
+	presets[1].freq = 0.27f;
+	presets[1].fat = -11.36f;
+	presets[1].drive = 0.53f;
 	presets[1].dry = 0.0f;
-	presets[1].stereo = 0.3f;
-	presets[1].gain = 0.4f;
+	presets[1].stereo = 0.0f;
 
-	presets[2].name = "Preset 2";
-	presets[2].freq = 0.3f;
-	presets[2].fat = 0.0f;
-	presets[2].drive = 0.0f;
-	presets[2].dry = 0.0f;
-	presets[2].stereo = 0.3f;
-	presets[2].gain = 0.4f;
+	presets[2].name = "Noisy Bass Pumper";
+	presets[2].freq = 0.55f;
+	presets[2].fat = 20.0f;
+	presets[2].drive = 0.59f;
+	presets[2].dry = 0.77f;
+	presets[2].stereo = 0.0f;
 
-	presets[3].name = "Preset 3";
-	presets[3].freq = 0.3f;
-	presets[3].fat = 0.0f;
-	presets[3].drive = 0.0f;
+	presets[3].name = "Broken Earphone";
+	presets[3].freq = 0.19f;
+	presets[3].fat = -1.92f;
+	presets[3].drive = 1.0f;
 	presets[3].dry = 0.0f;
-	presets[3].stereo = 0.3f;
-	presets[3].gain = 0.4f;
+	presets[3].stereo = 0.88f;
 
-	presets[4].name = "Preset 4";
-	presets[4].freq = 0.3f;
-	presets[4].fat = 0.0f;
-	presets[4].drive = 0.0f;
+	presets[4].name = "Fatass";
+	presets[4].freq = 0.62f;
+	presets[4].fat = -5.28f;
+	presets[4].drive = 1.0f;
 	presets[4].dry = 0.0f;
-	presets[4].stereo = 0.3f;
-	presets[4].gain = 0.4f;
+	presets[4].stereo = 0.0f;
 
-	presets[5].name = "Preset 5";
-	presets[5].freq = 0.3f;
-	presets[5].fat = 0.0f;
-	presets[5].drive = 0.0f;
+	presets[5].name = "Screaming Alien";
+	presets[5].freq = 0.63f;
+	presets[5].fat = 5.6f;
+	presets[5].drive = 0.2f;
 	presets[5].dry = 0.0f;
-	presets[5].stereo = 0.3f;
-	presets[5].gain = 0.4f;
+	presets[5].stereo = 0.0f;
 
-	presets[6].name = "Preset 6";
-	presets[6].freq = 0.3f;
-	presets[6].fat = 0.0f;
-	presets[6].drive = 0.0f;
+	presets[6].name = "Bad Connection";
+	presets[6].freq = 0.25f;
+	presets[6].fat = -2.56f;
+	presets[6].drive = 0.48f;
 	presets[6].dry = 0.0f;
-	presets[6].stereo = 0.3f;
-	presets[6].gain = 0.4f;
+	presets[6].stereo = 0.0f;
 
-	presets[7].name = "Preset 7";
-	presets[7].freq = 0.3f;
-	presets[7].fat = 0.0f;
+	presets[7].name = "Ouch";
+	presets[7].freq = 0.9f;
+	presets[7].fat = -20.0f;
 	presets[7].drive = 0.0f;
 	presets[7].dry = 0.0f;
-	presets[7].stereo = 0.3f;
-	presets[7].gain = 0.4f;
-
-	presets[8].name = "Preset 8";
-	presets[8].freq = 0.3f;
-	presets[8].fat = 0.0f;
-	presets[8].drive = 0.0f;
-	presets[8].dry = 0.0f;
-	presets[8].stereo = 0.3f;
-	presets[8].gain = 0.4f;
-
-	presets[9].name = "Preset 9";
-	presets[9].freq = 0.3f;
-	presets[9].fat = 0.0f;
-	presets[9].drive = 0.0f;
-	presets[9].dry = 0.0f;
-	presets[9].stereo = 0.3f;
-	presets[9].gain = 0.4f;
+	presets[7].stereo = 0.69f;
 }
 
 FmerAudioProcessor::~FmerAudioProcessor()
@@ -151,7 +127,7 @@ double FmerAudioProcessor::getTailLengthSeconds() const
 
 int FmerAudioProcessor::getNumPrograms()
 {
-	return 10;
+	return 8;
 }
 
 int FmerAudioProcessor::getCurrentProgram()
@@ -161,14 +137,14 @@ int FmerAudioProcessor::getCurrentProgram()
 
 void FmerAudioProcessor::setCurrentProgram (int index)
 {
+	if(!boot) return;
 	undoManager.beginNewTransaction((String)"Changed preset to " += presets[index].name);
 	currentpreset = index;
 	lerptable[0] = freq;
-	lerptable[1] = fat;
+	lerptable[1] = fat*.025f + .5f;
 	lerptable[2] = drive;
 	lerptable[3] = dry;
 	lerptable[4] = stereo;
-	lerptable[5] = gain;
 	lerpPreset(1);
 }
 void FmerAudioProcessor::lerpPreset(float stage)
@@ -176,20 +152,18 @@ void FmerAudioProcessor::lerpPreset(float stage)
 	stage *= .8;
 	if(stage < .001) {
 		apvts.getParameter("freq")->setValueNotifyingHost(presets[currentpreset].freq);
-		apvts.getParameter("fat")->setValueNotifyingHost(presets[currentpreset].fat*.025 + .5);
+		apvts.getParameter("fat")->setValueNotifyingHost(presets[currentpreset].fat*.025f + .5f);
 		apvts.getParameter("drive")->setValueNotifyingHost(presets[currentpreset].drive);
 		apvts.getParameter("dry")->setValueNotifyingHost(presets[currentpreset].dry);
 		apvts.getParameter("stereo")->setValueNotifyingHost(presets[currentpreset].stereo);
-		apvts.getParameter("gain")->setValueNotifyingHost(presets[currentpreset].gain);
 		undoManager.beginNewTransaction();
 		return;
 	}
 	lerpValue("freq", lerptable[0], presets[currentpreset].freq);
-	lerpValue("fat", lerptable[1], presets[currentpreset].fat*.025 + .5);
+	lerpValue("fat", lerptable[1], presets[currentpreset].fat*.025f + .5f);
 	lerpValue("drive", lerptable[2], presets[currentpreset].drive);
 	lerpValue("dry", lerptable[3], presets[currentpreset].dry);
 	lerpValue("stereo", lerptable[4], presets[currentpreset].stereo);
-	lerpValue("gain", lerptable[5], presets[currentpreset].gain);
 	new DelayedOneShotLambda(50.f/3.f, [this,stage]() { lerpPreset(stage); });
 }
 void FmerAudioProcessor::lerpValue(StringRef slider, float& oldval, float newval)
@@ -211,7 +185,6 @@ void FmerAudioProcessor::changeProgramName (int index, const juce::String& newNa
 	presets[index].drive = drive;
 	presets[index].dry = dry;
 	presets[index].stereo = stereo;
-	presets[index].gain = gain;
 }
 
 //==============================================================================
@@ -285,6 +258,8 @@ void FmerAudioProcessor::processBlock (juce::AudioBuffer<float>& buffer, juce::M
 	oldstereo = stereo;
 	oldgain = gain;
 	oldnorm = norm;
+
+	boot = true;
 }
 
 float FmerAudioProcessor::plasticfuneral(float source, int channel, float freq, float fat, float drive, float dry, float stereo, float gain)
@@ -321,16 +296,21 @@ void FmerAudioProcessor::getStateInformation (juce::MemoryBlock& destData)
 {
 	const char linebreak = '\n';
 	std::ostringstream data;
-	data << currentpreset << linebreak << freq << linebreak << fat << linebreak << drive << linebreak << dry << linebreak << stereo << linebreak << gain << linebreak;
+	data << version
+		<< linebreak << currentpreset
+		<< linebreak << freq
+		<< linebreak << fat
+		<< linebreak << drive
+		<< linebreak << dry
+		<< linebreak << stereo
+		<< linebreak << gain << linebreak;
 	for (int i = 0; i < getNumPrograms(); i++) {
-		data <<
-		presets[i].name << linebreak <<
-		presets[i].freq << linebreak <<
-		presets[i].fat << linebreak <<
-		presets[i].drive << linebreak <<
-		presets[i].dry << linebreak <<
-		presets[i].stereo << linebreak <<
-		presets[i].gain << linebreak;
+		data << presets[i].name
+			<< linebreak << presets[i].freq
+			<< linebreak << presets[i].fat
+			<< linebreak << presets[i].drive
+			<< linebreak << presets[i].dry
+			<< linebreak << presets[i].stereo << linebreak;
 	}
 	MemoryOutputStream stream(destData, false);
 	stream.writeString(data.str());
@@ -338,38 +318,40 @@ void FmerAudioProcessor::getStateInformation (juce::MemoryBlock& destData)
 
 void FmerAudioProcessor::setStateInformation (const void* data, int sizeInBytes)
 {
-	//a complete disaster but at least it works
 	std::stringstream ss(String::createStringFromData(data, sizeInBytes).toRawUTF8());
 	std::string token;
+
+	std::getline(ss, token, '\n');
+	int saveversion = std::stoi(token);
+
 	std::getline(ss, token, '\n');
 	currentpreset = std::stoi(token);
+
 	std::getline(ss, token, '\n');
 	apvts.getParameter("freq")->setValueNotifyingHost(std::stof(token));
+
 	std::getline(ss, token, '\n');
 	apvts.getParameter("fat")->setValueNotifyingHost(std::stof(token)*.025 + .5);
+
 	std::getline(ss, token, '\n');
 	apvts.getParameter("drive")->setValueNotifyingHost(std::stof(token));
+
 	std::getline(ss, token, '\n');
 	apvts.getParameter("dry")->setValueNotifyingHost(std::stof(token));
+
 	std::getline(ss, token, '\n');
 	apvts.getParameter("stereo")->setValueNotifyingHost(std::stof(token));
+
 	std::getline(ss, token, '\n');
 	apvts.getParameter("gain")->setValueNotifyingHost(std::stof(token));
+
 	for(int i = 0; i < getNumPrograms(); i++) {
-		std::getline(ss, token, '\n');
-		presets[i].name = token;
-		std::getline(ss, token, '\n');
-		presets[i].freq = std::stof(token);
-		std::getline(ss, token, '\n');
-		presets[i].fat = std::stof(token);
-		std::getline(ss, token, '\n');
-		presets[i].drive = std::stof(token);
-		std::getline(ss, token, '\n');
-		presets[i].dry = std::stof(token);
-		std::getline(ss, token, '\n');
-		presets[i].stereo = std::stof(token);
-		std::getline(ss, token, '\n');
-		presets[i].gain = std::stof(token);
+		std::getline(ss, token, '\n'); presets[i].name = token;
+		std::getline(ss, token, '\n'); presets[i].freq = std::stof(token);
+		std::getline(ss, token, '\n'); presets[i].fat = std::stof(token);
+		std::getline(ss, token, '\n'); presets[i].drive = std::stof(token);
+		std::getline(ss, token, '\n'); presets[i].dry = std::stof(token);
+		std::getline(ss, token, '\n'); presets[i].stereo = std::stof(token);
 	}
 }
 
@@ -384,11 +366,11 @@ AudioProcessorValueTreeState::ParameterLayout
 	FmerAudioProcessor::createParameters()
 {
 	std::vector<std::unique_ptr<RangedAudioParameter>> parameters;
-	parameters.push_back(std::make_unique<AudioParameterFloat>("freq","Frequency",0.0f,1.0f,0.3f));
+	parameters.push_back(std::make_unique<AudioParameterFloat>("freq","Frequency",0.0f,1.0f,0.32f));
 	parameters.push_back(std::make_unique<AudioParameterFloat>("fat","Fatness",-20.0f,20.0f,0.0f));
 	parameters.push_back(std::make_unique<AudioParameterFloat>("drive","Drive",0.0f,1.0f,0.0f));
 	parameters.push_back(std::make_unique<AudioParameterFloat>("dry","Dry",0.0f,1.0f,0.0f));
-	parameters.push_back(std::make_unique<AudioParameterFloat>("stereo","Stereo",0.0f,1.0f,0.3f));
+	parameters.push_back(std::make_unique<AudioParameterFloat>("stereo","Stereo",0.0f,1.0f,0.37f));
 	parameters.push_back(std::make_unique<AudioParameterFloat>("gain","Out Gain",0.0f,1.0f,0.4f));
 	return { parameters.begin(), parameters.end() };
 }
