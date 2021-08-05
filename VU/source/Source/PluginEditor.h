0824833c61c9400e7f13137b23d901d90eec7e4e
melody <melody@unplug.red>
2021-08-05 21:04:40 +0300

    unified display and editor in vu

--- VU/source/Source/PluginEditor.h
+++ VU/source/Source/PluginEditor.h
@@ -9,21 +9,59 @@
 #pragma once
 #include <JuceHeader.h>
 #include "PluginProcessor.h"
-#include "display.h"
+#include "functions.h"
 using namespace juce;
 
 //==============================================================================
 /**
 */
-class VuAudioProcessorEditor  : public AudioProcessorEditor, private Timer
+class VuAudioProcessorEditor  : public AudioProcessorEditor, public OpenGLRenderer, private Timer
 {
 public:
 	VuAudioProcessorEditor (VuAudioProcessor&);
 	~VuAudioProcessorEditor() override;
 
-	//==============================================================================
+	void newOpenGLContextCreated() override;
+	void renderOpenGL() override;
+	void openGLContextClosing() override;
 	void paint (Graphics&) override;
 	void resized() override;
+
+	void mouseEnter(const MouseEvent& event) override;
+	void mouseMove(const MouseEvent& event) override;
+	void mouseExit(const MouseEvent& event) override;
+	void mouseDown(const MouseEvent& event) override;
+	void mouseDrag(const MouseEvent& event) override;
+	void mouseUp(const MouseEvent& event) override;
+	int recalchover(float x, float y);
+
+	float leftvu = 0;
+	float rightvu = 0;
+	float leftrms = 0;
+	float rightrms = 0;
+	float leftvelocity = 0;
+	float rightvelocity = 0;
+	float multiplier = 1;
+
+	bool leftpeak = false;
+	bool rightpeak = false;
+	float leftpeaklerp = 0;
+	float rightpeaklerp = 0;
+	float leftpeakvelocity = 0;
+	float rightpeakvelocity = 0;
+	int lefthold = 0;
+	int righthold = 0;
+
+	float stereodamp = 0;
+	int settingstimer = 0;
+	float settingsfade = 0;
+	float settingsvelocity = 0;
+	int hover = 0;
+	int initialdrag = 0;
+	bool held = false;
+	int initialvalue = 0;
+	float stereovelocity = 0;
+	float websiteht = 0;
 
 	void timerCallback() override;
 
@@ -31,7 +69,21 @@ public:
 	int prevw = 0;
 private:
 	VuAudioProcessor& audioProcessor;
-	displayComponent displaycomp;
 
-	JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (VuAudioProcessorEditor)
+	OpenGLContext openGLContext;
+	unsigned int arraybuffer;
+	OpenGLTexture vutex;
+	OpenGLTexture mptex;
+	OpenGLTexture lgtex;
+	float square[8]{
+		0.f,0.f,
+		1.f,0.f,
+		0.f,1.f,
+		1.f,1.f};
+
+	std::unique_ptr<OpenGLShaderProgram> vushader;
+	String vertshader;
+	String fragshader;
+
+	JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR(VuAudioProcessorEditor)
 };
