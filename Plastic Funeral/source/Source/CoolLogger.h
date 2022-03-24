287190a54b097f4c2922c05f73b44678fde38f62
unplugred <AriHanan@pm.me>
2022-03-24 22:24:48 +0200

    updated logger in pf and proto

--- Plastic Funeral/source/Source/CoolLogger.h
+++ Plastic Funeral/source/Source/CoolLogger.h
@@ -11,6 +11,7 @@
 #pragma once
 
 #define ENABLE_CONSOLE
+#define ENABLE_TEXT
 #define CONSOLE_LENGTH 20
 
 #include <JuceHeader.h>
@@ -23,23 +24,26 @@ public:
 	void debug(float str, bool timestamp = true);
 	void debug(double str, bool timestamp = true);
 	void debug(bool str, bool timestamp = true);
-	void drawstring(String txty, float x = .5, float y = .5, float xa = .5f, float ya = .5f);
+	void drawstring(String txty, float x = .5, float y = .5, float xa = .5f, float ya = .5f, std::unique_ptr<OpenGLShaderProgram>* shader = nullptr);
 	void drawlog();
 	void release();
 
+#ifdef ENABLE_TEXT
+	String textvert;
+	String textfrag;
+#endif
 private:
+#ifdef ENABLE_TEXT
+	OpenGLTexture texttex;
+	std::unique_ptr<OpenGLShaderProgram> textshader;
+	OpenGLContext* context;
+	int width = 100;
+	int height = 100;
+#endif
 #ifdef ENABLE_CONSOLE
 	String debuglist[CONSOLE_LENGTH];
 	int debugreadpos = 0;
 	std::mutex debugmutex;
 	String debugtxt = "";
-
-	OpenGLTexture texttex;
-	std::unique_ptr<OpenGLShaderProgram> textshader;
-	String textvert;
-	String textfrag;
-	OpenGLContext* context;
-	int width = 100;
-	int height = 100;
 #endif
 };
