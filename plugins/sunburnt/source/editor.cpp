185aa2b5421bcabc49b83b22e1b4c1b72b56de4f
melody <melody@unplug.red>
2025-03-20 08:34:56 +0200

    (sunburnt) fixed font not present 3

--- plugins/sunburnt/source/editor.cpp
+++ plugins/sunburnt/source/editor.cpp
@@ -1746,7 +1746,7 @@ void LookNFeel::drawPopupMenuItem(Graphics &g, const Rectangle<int> &area, bool 
 	float maxFontHeight = ((float)r.getHeight())/1.45f;
 	if(((int)text.toRawUTF8()[1]) < 0) {
 		if(CJKFont == "None")
-			return Font(20.f*scale,Font::plain);
+			font = Font(20.f*scale,Font::plain);
 		font = Font(CJKFont,"Regular",20.f*scale);
 		maxFontHeight = r.getHeight();
 	}
